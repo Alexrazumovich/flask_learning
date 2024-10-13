50346e3be0f0e95b7c548f84146dcdeb85610c83
Alexrazumovich <alexrazumovich@gmail.com>
2024-10-13 12:57:59 +0300

    add_shablon

--- main.py
+++ main.py
@@ -3,13 +3,28 @@ from flask import Flask,render_template
 app=Flask(__name__)
 
 
-@app.route('/')
-def films():
+# @app.route('/')
+# def films():
+#     context={
+#         'caption': 'Фильмы',
+#         'number':1,
+#         'list':['Nina','Alex','Anton','Nikita']
+#
+#     }
+#     return render_template('shablon.html',**context)
 
-    return render_template('index2.html')
+@app.route('/')
+def films2():
+    context={
+        'link':'Фильмы'
+    }
+    return render_template('index2.html',**context)
 @app.route('/person/')
 def person():
-    return render_template('main.html')
+    context={
+        'link':'Герои фильмов'
+    }
+    return render_template('main.html',**context)
 
 if __name__ == '__main__':
     app.run()
